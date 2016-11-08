0b8f0f63a8240d1d5df22871e1f24a86839c04ea
Christopher Bruner <chrbruner9@gmail.com>
2016-11-07 21:40:56 -0800

    have api now working using koa where you must return a yield statement on the body

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 const app = require('./lib/app');
 require('./lib/mongoose-setup');
 
-const server = http.createServer(app);
+const server = http.createServer(app.callback());
 server.listen(port, () => {
     console.log('server currently connected to port', port);
 });
---
+++ test/animekoadb.api.test.js
@@ -0,0 +1,84 @@
+const chai = require('chai');
+const assert = chai.assert;
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const connection = require('../lib/mongoose-setup');
+const app = require('../lib/app')
+
+describe('tests the animechara and animeshows api using a koa framework', () => {
+   before( done => {
+        const CONNECTED = 1;
+        if (connection.readyState === CONNECTED) dropCollection();
+        else connection.on('open', dropCollection);
+
+        function dropCollection() {
+            const animeshow = 'animeshows';
+            const animechar = 'animechars';
+            connection.db
+                .listCollections({name: [animeshow, animechar]})
+                .next((err, callinfo) => {
+                    if (!callinfo) return done();
+                    connection.db.dropCollection([animeshow, animechar], done);
+                });
+        };
+    });
+
+    const request = chai.request(app.callback());
+    const keiichi = {
+        name: 'Keiichi Maebara',
+        age: 16,
+        power: 'Breaking Fate',
+        attackpower: 8,
+        hair_color: 'brown',
+    };
+    const higurashi = {
+        showname: 'Higurashi no Naku Koro Ni',
+        airdate: '2006-04-04',
+        genre: 'mystery horror',
+    };
+    let show_id = '';
+
+    it('makes a /GET request on all anime chars', done => {
+        request
+            .get('/animechars')
+            .then(res => {
+                assert.isOk(res.body);
+                done();
+            })
+            .catch(err => {
+                console.error(err);
+                done(err);
+            });
+    });
+
+    it('makes a /POST request to anime chars using Keiichi', done => {
+        request
+            .post('/animechars')
+            .set('content', JSON.stringify(keiichi))
+            .then(res => {
+                const character = res.body;
+                assert.isOk(character._id);
+                keiichi._id = character._id;
+                keiichi.__v = 0;
+                assert.deepEqual(character, keiichi);
+                done();
+            })
+            .catch(err => {
+                console.error(err);
+                done(err);
+            });
+    });
+
+    it('makes a /GET request for Keiichi', done => {
+        request
+            .get('/animechars/' + keiichi._id)
+            .then(res => {
+                assert.deepEqual(res.body, keiichi);
+                done();
+            })
+            .catch(err => {
+                console.error(err);
+                done(err);
+            });
+    });
+});
